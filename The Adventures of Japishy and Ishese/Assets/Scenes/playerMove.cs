90cfae863c9b979e02445a37eaafe8156de115a6
burntbacon99 <50713170+burntbacon99@users.noreply.github.com>
2019-05-16 13:52:00 -0400

    jump

---
+++ The Adventures of Japishy and Ishese/Assets/Scenes/playerMove.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMove : MonoBehaviour
+{
+
+    public int moveX;
+    public bool facingRight = true;
+    public int playerSpeed = 10;
+    public bool player1 = true;
+    public int jumpPower = 500;
+    private Rigidbody2D myRigidbody;
+
+
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+
+        
+    }
+
+    void Move()
+    {
+
+        if (player1)
+        {
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                jump();
+            }
+
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                jump();
+            }
+
+        }
+
+    }
+
+    void jump()
+    {
+        GetComponent<Rigidbody2D>().AddForce(Vector2.up * jumpPower);
+
+    }
+}
