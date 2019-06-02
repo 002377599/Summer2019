ca30b431629be4d256a068c9c881d9452f6502d9
burntbacon99 <50713170+burntbacon99@users.noreply.github.com>
2019-06-01 21:49:43 -0400

    camera
    i fixed the camera

--- The Adventures of Japishy and Ishese/Assets/Scripts/MovingCamera.cs
+++ The Adventures of Japishy and Ishese/Assets/Scripts/MovingCamera.cs
@@ -5,24 +5,34 @@ using UnityEngine;
 public class MovingCamera : MonoBehaviour
 {
     private GameObject player;
+    private GameObject player2;
     public float xmin;
     public float xmax;
     public float ymin;
     public float ymax;
 
+
     // Start is called before the first frame update
     void Start()
     {
-        player = GameObject.FindWithTag("Player"); 
+
+        player = GameObject.FindWithTag("Player");
+        player2 = GameObject.FindWithTag("Player2");
         
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        //player1
         float x = Mathf.Clamp(player.transform.position.x,xmin,xmax);
         float y = Mathf.Clamp(player.transform.position.y, ymin, ymax);
-        gameObject.transform.position = new Vector3(x, y, gameObject.transform.position.z);
 
+        //player2
+        float x2 = Mathf.Clamp(player2.transform.position.x, xmin, xmax);
+        float y2 = Mathf.Clamp(player2.transform.position.y, ymin, ymax);
+
+        gameObject.transform.position = new Vector3((x+x2)/2, (y+y2)/2, gameObject.transform.position.z);
     }
 }
